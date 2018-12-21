393b70fcc45292797911e288b33b8977140d6756
RamonJul <ramoncarlosjulian@gmail.com>
2018-12-21 14:34:27 -0500

    Final

--- assets/javscript/app.js
+++ assets/javscript/app.js
@@ -3,53 +3,46 @@ $(document).ready(function () {
   var term
   var rating
   var limit
-  var theme = [];
+  var theme
+  if (JSON.parse(localStorage.getItem("theme")) === null) {
+    theme = []
+  } else {
+    theme = JSON.parse(localStorage.getItem("theme"))
+  }
+  var width = window.matchMedia("(max-width:575px)")
 
-  function grab_gifs(purpose) {
+  function grab_gifs() {
     var apikey = "lithhVQDOaavEcxQbEX0Vywb6Bb60syr"
     var queryUrl = "http://api.giphy.com/v1/gifs/search"
-    console.log(limit)
-    console.log(rating)
     queryUrl += "?" + $.param({
       "q": term,
       "api_key": apikey,
       "limit": limit,
       "rating": rating
     })
-    console.log(queryUrl)
     img_holder.empty()
 
     $.ajax({
         url: queryUrl,
         method: 'GET',
       }).then(function (response) {
-        if (purpose === "check") {
-          if (response.length === 0) {
-            return false;
 
-          }
-        } else {
-
-          for (var i = 0; i < response.data.length; i++) {
-            var gif = $("<img>")
-            gif.addClass("gif")
-            gif.attr("still", response.data[i].images.fixed_height_still.url)
-            gif.attr("moving", response.data[i].images.fixed_height.url)
-            gif.attr("state", "active")
-            gif.attr("src", response.data[i].images.fixed_height.url)
-            img_holder.append(gif)
-            console.log(gif.attr("state"))
-          }
-          $(".gif").on("click", pause_gifs)
+        for (var i = 0; i < response.data.length; i++) {
+          var gif = $("<img>")
+          gif.addClass("gif")
+          gif.attr("still", response.data[i].images.fixed_height_still.url)
+          gif.attr("moving", response.data[i].images.fixed_height.url)
+          gif.attr("state", "active")
+          gif.attr("src", response.data[i].images.fixed_height.url)
+          img_holder.append(gif)
         }
 
       })
-      .fail(function (err) {
-        throw err;
+      .catch(function (err) {
+
       })
   }
-
-  function pause_gifs() {
+  $(document).on("click", ".gif", function () {
     var that = $(this)
     if (that.attr("state") === "active") {
       that.attr("src", that.attr("still"))
@@ -59,74 +52,61 @@ $(document).ready(function () {
       that.attr("state", "active")
 
     }
-  }
+
+  })
 
   function repeat_check(check) {
     var no_repeat = true
     for (var i = 0; i < theme.length; i++) {
-      if (check === theme[i].attr("data-search"))
+      if (check === theme[i])
         no_repeat = false
     }
-    console.log(no_repeat)
     return no_repeat;
   }
 
-  // function hits(check) {
-  //   var hit = true
-  //   term = check
-  //   var apikey = "lithhVQDOaavEcxQbEX0Vywb6Bb60syr"
-  //   var queryUrl = "http://api.giphy.com/v1/gifs/search"
-  //   queryUrl += "?" + $.param({
-  //     "q": term,
-  //     "api_key": apikey,
-  //   })
-  //   console.log(queryUrl)
-  //   img_holder.empty()
-  //   $.ajax({
-  //     url: queryUrl,
-  //     method: 'GET',
-  //   }).then(function (response) {
-  //     console.log(response)
-  //     console.log(response.data.length)
-   
-  //     if (response.data.length === 0) {
-  //       hit = false
-  //     }
-  //     rating = null;
-  //     term = null;
-  //   return hit;
-     
-  //   })
-  //   console.log(hit)
-  //   return hit;
-  // }
+  function render_buttons() {
+    $("#button_holder").empty();
+    for (var i = 0; i < theme.length; i++) {
+
+      var term_holder = $("<button>")
+      term_holder.addClass("search_term button btn btn-outline-primary")
+      term_holder.attr("data-search", theme[i])
+      term_holder.text(theme[i])
+
+      $("#button_holder").append(term_holder)
+      if (width.matches) {
+        height_adjust()
+
+      }
+    }
+
+  }
+
+  function height_adjust() {
+    var height = $("#theme_list").css("height");
+    var top = $("#sidebar").css("top")
+    if (height !== top) {
+      $("#sidebar").css("top", height)
+    }
+  }
+
+
+
+
   $("#add_theme").on("click", function () {
     var addition = $("#theme_new").val().trim();
     $("#theme_new").val("Theme")
-    // console.log(addition.length)
-    // console.log(hits(addition))
-    // && hits(addition)
-    if (addition.length != 0 && repeat_check(addition) ) {
-      console.log(1)
-      $("#button_holder").empty();
-      var term_holder = $("<button>")
-      term_holder.addClass("search_term button btn btn-outline-primary")
-      term_holder.attr("data-search", addition)
-      term_holder.text(addition)
-      // theme=JSON.parse(localStorage.getItem("theme"))
-      theme.push(term_holder)
-      // localStorage.setItem("theme", JSON.stringify(theme))
-      for (var i = 0; i < theme.length; i++) {
-        console.log(theme[i])
-        $("#button_holder").append(theme[i])
-      }
+    if (addition.length != 0 && repeat_check(addition)) {
+      theme.push(addition)
+      localStorage.setItem("theme", JSON.stringify(theme))
+      render_buttons();
     }
   })
 
 
   $("#filter").on("click", function () {
 
-    console.log($("option:selected").attr("value"))
+
     limit = $("#limit_info").val().trim()
     rating = $("option:selected").attr("value")
     grab_gifs("display")
@@ -136,7 +116,8 @@ $(document).ready(function () {
     limit = null
     rating = null
     $("#limit_info").val("Limit")
-    $("#inputGroupSelect01").prop("selectedIndex",0)
+    $("#inputGroupSelect01 option[value=reset]").attr('selected', 'selected');
+
 
   })
   $("#clear_gif").on("click", function () {
@@ -148,6 +129,34 @@ $(document).ready(function () {
     grab_gifs()
   });
 
+  $("#clear_button").on("click", function () {
+    $("#button_holder").empty();
+    localStorage.clear()
+    theme = []
 
-  // localStorage.clear()
+  })
+
+
+  if (screen.width < 576) {
+    height_adjust()
+    $(".navbar-toggler").attr("disabled", false)
+  } else {
+    $(".navbar-toggler").attr("disabled", true)
+  }
+  $(window).resize(function () {
+
+    if (screen.width < 576) {
+      height_adjust()
+      $(".navbar-toggler").attr("disabled", false)
+    } else {
+      $("#sidebar").css("top", 0)
+      $(".navbar-toggler").attr("disabled", true)
+      if ($(".collapse").hasClass("show") !== true) {
+        $(".collapse").addClass("show")
+      }
+    }
+
+  })
+
+  render_buttons();
 })
